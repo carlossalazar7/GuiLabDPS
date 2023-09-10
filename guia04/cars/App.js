cac9985b0e3f2e86ab516013b4c3a672f9dc507a
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2023-09-09 20:58:18 -0600

    mod 09-09-2023

--- guia04/cars/App.js
+++ guia04/cars/App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { SafeAreaView, View, FlatList, Text } from 'react-native';
+import { styles } from './styles';
+import { DATA } from './src/data';
+
+const Item = ({ title , img}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+    <Image style={styles.img}  source={img} />
+  </View>
+);
 
 export default function App() {
+  const renderItem = ({ item }) => (
+    <Item title={item.title} img={item.src} />
+  );
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ guia04/cars/src/data.js
@@ -0,0 +1,17 @@
+export const DATA = [
+  {
+    id: '1',
+    title: 'Toyota',
+    src:require('./imgs/toyota.jpg'),
+  },
+  {
+    id: '2',
+    title: 'Mazda',
+    src:require('./src/imgs/mazda.jpg'),
+  },
+  {
+    id: '3',
+    title: 'Mitsubishi',
+    src:require('./src/imgs/mitsubishi.jpeg'),
+  },
+];
